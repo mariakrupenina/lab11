3ad40c20be49911d15fe9568ec3e2b11142bab58
user <user@WIN-RIQ72S9838G>
2024-04-07 12:57:53 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using library_for_lab10;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace lab11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                    ЗАДАНИЕ 1");
+            //Создание универсальной коллекции для хранения объектов иерархии классов
+            ArrayList tools = new ArrayList();
+
+            //Добавление объектов в коллекцию
+            for (int i = 0; i < 5; i++)
+            {
+                Tool t = new Tool();
+                tools.Add(t);
+                t.RandomInit();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                HandTool h = new HandTool();
+                tools.Add(h);
+                h.RandomInit();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                ElectricTool e = new ElectricTool();
+                tools.Add(e);
+                e.RandomInit();
+            }
+
+            tools.Sort();
+            foreach (object t in tools)
+            {
+                Console.WriteLine(t.ToString());
+            }
+
+            Console.WriteLine(tools.Count);
+            Console.WriteLine(tools.Capacity);
+
+            //Поиск и удаление
+            Console.WriteLine("Введите элемент для удаления");
+            Tool find = new Tool();
+            find.Init();
+            int pos = tools.IndexOf(find);
+            if (pos >= 0)
+            {
+                Console.WriteLine($"Удаляем {tools[pos]}");
+                tools.RemoveAt(pos);
+            }
+            else
+                Console.WriteLine("Невозможно удалить!нет такого инстурмента!");
+
+            Console.WriteLine();
+
+            //Запросы к коллекции
+            //1.Количество элементов определенного вида
+            Console.WriteLine("1. Количество элементов определенного вида");
+            int handCount1 = 0;
+            int toolCount1 = 0;
+            int electricCount1 = 0;
+            foreach (Tool item in tools)
+            {
+                if (item.GetType() == typeof(HandTool))
+                {
+                    handCount1++;
+                }
+                else if (item.GetType() == typeof(ElectricTool))
+                {
+                    electricCount1++;
+                }
+                else if (item.GetType() == typeof(Tool))
+                {
+                    //Исключаем HandTool и ElectricTool(они прописаны выше)
+                    if (!(item is HandTool) && !(item is ElectricTool))
+                    {
+                        toolCount1++;
+                    }
+                }
+            }
+            Console.WriteLine($"Количество ручных инстурментов: {handCount1}");
+            Console.WriteLine($"Количество иинструментов: {toolCount1}");
+            Console.WriteLine($"Количество электрических иинструментов: {toolCount1}");
+
+            Console.WriteLine();
+
+            //2.Печать элементов определенного вида
+            Console.WriteLine("2. Печать элементов определенного вида");
+            Console.WriteLine("Ручные инструменты:");
+            foreach (object obj in tools)
+            {
+                if (obj.GetType() == typeof(HandTool))
+                {
+                    Console.WriteLine(obj.ToString());
+                }
+            }
+            Console.WriteLine();
+
+            //Пример из 10лабы
+            //3.Максимальное время работы аккумуляторного электрического инструмента
+            Console.Write("3. Максимальное время работы аккумуляторного электрического инструмента: ");
+
+            ElectricTool e3 = new ElectricTool("батарея", "железо", "аккумулятор", 5, 5);
+            tools.Add(e3);
+
+            double maxValue = 0;
+            foreach (object item in tools)
+            {
+                if (item is ElectricTool)
+                {
+                    ElectricTool electricTool = (ElectricTool)item;
+                    if (electricTool.PowerSource == "аккумулятор" && electricTool.BatteryTime > maxValue)
+                    {
+                        maxValue = electricTool.BatteryTime;
+                    }
+                }
+            }
+
+            if (maxValue > 0)
+            {
+                Console.WriteLine($"{maxValue} мин.");
+            }
+
+            else
+            {
+                Console.WriteLine("не определено, так как инструментов с такими условиями нет.");
+            }
+
+            Console.WriteLine();
+
+            ////Перебор элементов коллекции с помощью foreach
+            //Console.WriteLine("Все инструменты:");
+            //foreach (object t4 in tools)
+            //{
+            //    t4.ToString();
+            //}
+
+            //Клонирование коллекции
+            ArrayList cloned = (ArrayList)tools.Clone();
+            //foreach (object t in cloned)
+            //{
+            //    Console.WriteLine(t.ToString());
+            //}
+
+            // Глубокое копирование коллекции tools
+            //ArrayList cloned = DeepCopy(tools);
+            //// Вывод клонированной коллекции для проверки
+            //Console.WriteLine("Клонированная коллекция:");
+            //foreach (object t in cloned)
+            //{
+            //    Console.WriteLine(t.ToString());
+            //}
+
+
+            //Сортировка коллекции и поиск элемента
+            tools.Sort();
+            Tool searchTool = new Tool();
+            Console.WriteLine("ПОИСК ЭЛЕМЕНТА");
+            searchTool.Init();
+            //Находим индекс объекта в коллекции
+            int index = tools.IndexOf(searchTool);
+
+            if (index != -1)
+            {
+                Console.WriteLine($"Элемент найден по индексу: {index+1}");
+            }
+            else
+            {
+                Console.WriteLine("Элемент не найден.");
+            }
+
+
+            //Задание 2
+
+            Console.WriteLine("                    ЗАДАНИЕ 2");
+
+            LinkedList<Tool> tools2 = new LinkedList<Tool>();
+
+            //Добавление объектов в коллекцию
+            for (int i = 0; i < 5; i++)
+            {
+                Tool t = new Tool();
+                t.RandomInit();
+                tools2.AddFirst(new LinkedListNode<Tool>(t));
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                HandTool h = new HandTool();
+                tools2.AddFirst(h);
+                h.RandomInit();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                ElectricTool e = new ElectricTool();
+                tools2.AddLast(e);
+                e.RandomInit();
+            }
+            //Вывод элементов списка
+            foreach (Tool t in tools2)
+            {
+                Console.WriteLine(t.ToString()); 
+            }
+
+            Console.WriteLine($"Количество инструментов = {tools2.Count}");
+
+            // Клонирование коллекции tools(глубокое не требуется, элементы хранятся по значению(не по ссылке))
+            LinkedList<Tool> cloned2 = new LinkedList<Tool>(tools2);
+
+            //Поиск и удаление
+            Console.WriteLine("Введите элемент для удаления. Сначала найдём его.Введите элемент: ");
+            Tool find2 = new Tool();
+            find2.Init(); 
+            if (tools2.Contains(find2))
+                Console.WriteLine("Найден");
+            else
+                Console.WriteLine("Не найден");
+            LinkedListNode<Tool> node = tools2.Find(find2);
+            if (node != null)
+            {
+                Console.WriteLine($"Удаляем {node.Value}");
+                tools.Remove(node);
+            }
+
+            Console.WriteLine();
+
+            // Запросы к коллекции
+            // 1. Количество элементов определенного вида в изначальном массиве (без удаления, если было)
+            Console.WriteLine("1. Количество элементов определенного вида");
+            int handCount2 = 0;
+            int toolCount2 = 0;
+            int electricCount2 = 0;
+
+            foreach (Tool item in cloned2)
+            {
+                Type type = item.GetType();
+                if (type == typeof(HandTool))
+                {
+                    handCount2++;
+                }
+                else if (item is ElectricTool)
+                {
+                    electricCount2++;
+                }
+                else if (item is Tool)
+                {
+                    toolCount2++;
+                }
+            }
+
+            Console.WriteLine($"Количество ручных инструментов: {handCount2}");
+            Console.WriteLine($"Количество инструментов: {toolCount2}");
+            Console.WriteLine($"Количество электрических инструментов: {electricCount2}");
+
+            Console.WriteLine();
+
+            // 2. Печать элементов определенного вида
+            Console.WriteLine("2. Печать элементов определенного вида");
+            Console.WriteLine("Ручные инструменты:");
+            foreach (Tool t2 in cloned2)
+            {
+                Type type = t2.GetType();
+                if (type == typeof(HandTool))
+                {
+                    Console.WriteLine(t2.ToString());
+                }
+            }
+
+            //3.Максимальное время работы аккумуляторного электрического инструмента
+            Console.Write("3. Максимальное время работы аккумуляторного электрического инструмента: ");
+
+            ElectricTool toolAdd = new ElectricTool("батарея", "железо", "аккумулятор", 45, 5);
+            cloned2.AddLast(toolAdd);
+            double maxValue2 = 0;
+            foreach (Tool item in cloned2)
+            {
+                if (item is ElectricTool electricTool && electricTool.PowerSource == "аккумулятор" && electricTool.BatteryTime >= maxValue2)
+                {
+                    maxValue2 = electricTool.BatteryTime;
+                }
+            }
+            if (maxValue2 != 0)
+                Console.WriteLine($"{maxValue2} мин.");
+            else
+                Console.WriteLine("Не определена, так как инструментов с такими условиями нет");
+
+
+            Console.WriteLine();
+            //Перебор элементов коллекции с помощью foreach
+            //Console.WriteLine("Все инструменты:");
+            //foreach (Tool t4 in tools2)
+            //{
+            //    t4.Print();
+            //}
+
+
+            //Сортировка списка
+            // Копируем элементы из LinkedList<Tool> в List<Tool>
+            List<Tool> tempList = new List<Tool>(tools2);
+            tempList.Sort();
+            //Обновление элементов в исходной LinkedList<Tool>, отсортированными элементами из List<Tool>
+            var currentNode = tools2.First; //инициализируется как первый элемент в tools2 "текущая"
+            foreach (var item in tempList)
+            {
+                currentNode.Value = item;
+                currentNode = currentNode.Next;
+            }
+
+            Console.WriteLine("Отсортированная коллекция:");
+            foreach (var item in tools2)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+
+            //Поиск в отсортированном листе(в базовом классе переопределены хеш и иквалс)
+            Tool find3 = new Tool();
+            find3.Init();
+            if (cloned2.Contains(find3))
+                Console.WriteLine("Найден");
+            else
+            {
+                Console.WriteLine("Не найден");
+            }
+
+            Console.WriteLine("                    ЗАДАНИЕ 3");
+            int numberOfElements = 1000;
+            TestCollections collections = new TestCollections(numberOfElements);
+            Console.WriteLine($"Количество элементов в handToolQueue: {collections.GetHandToolQueueCount()}");
+            Console.WriteLine($"Количество элементов в stringQueue: {collections.GetStringQueueCount()}");
+            Console.WriteLine($"Количество элементов в toolHashSet: {collections.GetToolHashSetCount()}");
+            Console.WriteLine($"Количество элементов в stringHashSet: {collections.GetStringHashSetCount()}");
+
+            if (collections.GetHandToolQueueCount() == collections.GetStringQueueCount() && collections.GetStringQueueCount() == collections.GetToolHashSetCount() && collections.GetToolHashSetCount() == collections.GetStringHashSetCount())
+                Console.WriteLine($"Ура! Коллекции уникальны и в каждой {collections.GetHandToolQueueCount()} элементов");
+            //// Вывод элементов коллекций
+            //testCollections.PrintToolQueue();
+            //testCollections.PrintToolHashSet();
+
+            Console.WriteLine("Измерение времени коллекций:");
+            TestCollections collection = new TestCollections(1000);
+            collection.MeasureTime();
+        }
+    }
+}
---
+++ TestCollections.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using library_for_lab10;
+
+namespace lab11
+{
+    public class TestCollections
+    {
+        HandTool first, middle, last;
+
+        Queue<HandTool> handToolQueue = new Queue<HandTool>();
+        Queue<string> stringQueue = new Queue<string>();
+        HashSet<Tool> toolHashSet = new HashSet<Tool>();
+        HashSet<string> stringHashSet = new HashSet<string>();
+
+        
+        public TestCollections(int numberOfElements)
+        {
+            int count = 0; // Счетчик добавленных элементов
+
+            while (count < numberOfElements)
+            {
+                try
+                {
+                    HandTool handTool = new HandTool();
+                    handTool.RandomInit();
+
+                    string toolString = handTool.ToString(); //строковое представление базового класса
+
+                    //Проверяем наличие рандомного элемента в уникальных коллекциях
+                    if (!toolHashSet.Contains(handTool) && !stringHashSet.Contains(toolString))
+                    {
+                        toolHashSet.Add(handTool.GetBase);
+                        stringHashSet.Add(toolString);
+                        stringQueue.Enqueue(toolString);
+                        handToolQueue.Enqueue(handTool);
+
+                        count++; //Увеличиваетя счетчик только если элемент был успешно добавлен
+                    }
+
+                    if (count == 1)
+                        first = (HandTool)handTool;
+                    if (count == numberOfElements / 2)
+                        middle = (HandTool)handTool;
+                    if (count == numberOfElements - 1)
+                        last = (HandTool)handTool;
+                }
+                catch
+                {
+     //
+                }
+            }
+        }
+
+
+        // Метод для вывода элементов очереди на экран
+        public void PrintToolQueue()
+        {
+            Console.WriteLine("Элементы в очереди:");
+            foreach (var tool in handToolQueue)
+            {
+                Console.WriteLine(tool);
+            }
+
+            Console.WriteLine(handToolQueue.Count());
+        }
+
+        // Метод для вывода элементов хеш-множества на экран
+        public void PrintToolHashSet()
+        {
+            Console.WriteLine("Элементы в хеш-множестве (HashSet<Tool>):");
+            foreach (var tool in toolHashSet)
+            {
+                Console.WriteLine(tool);
+            }
+            Console.WriteLine($"Количество инструментов: {toolHashSet.Count()}");
+        }
+
+        public int GetHandToolQueueCount()
+        {
+            return handToolQueue.Count;
+        }
+
+        public int GetStringQueueCount()
+        {
+            return stringQueue.Count;
+        }
+
+        public int GetToolHashSetCount()
+        {
+            return toolHashSet.Count;
+        }
+
+        public int GetStringHashSetCount()
+        {
+            return stringHashSet.Count;
+        }
+
+        public void MeasureTime()
+        {
+            MeasureSearchTime(first, "Первый");
+            MeasureSearchTime(middle, "Средний");
+            MeasureSearchTime(last, "Последний");
+            MeasureSearchTime(new HandTool(), "Несуществующий");
+        }
+
+        private void MeasureSearchTime(HandTool tool, string description)
+        {
+            Console.WriteLine($"Поиск {description} элемента");
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            bool isFindInHandToolQueue = handToolQueue.Contains(tool);
+            bool isFindInStringQueue = stringQueue.Contains(tool.ToString());
+            bool isFindInToolHashSet = toolHashSet.Contains(tool.GetBase);
+            bool isFindInStringHashSet = stringHashSet.Contains(tool.ToString());
+
+            sw.Stop();
+
+            PrintSearchResult(isFindInHandToolQueue, sw.ElapsedTicks);
+            PrintSearchResult(isFindInStringQueue, sw.ElapsedTicks);
+            PrintSearchResult(isFindInToolHashSet, sw.ElapsedTicks);
+            PrintSearchResult(isFindInStringHashSet, sw.ElapsedTicks);
+        }
+
+        private void PrintSearchResult(bool isFound, long elapsedTicks)
+        {
+            if (isFound)
+                Console.WriteLine($"Элемент найдён за {elapsedTicks}");
+            else
+                Console.WriteLine($"Элемент не найдён за {elapsedTicks}");
+        }
+    }
+    }
